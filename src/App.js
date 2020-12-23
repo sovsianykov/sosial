65b12c328bf55289e830c89944d2a652ea97b932
sovsianykov <swebproger@gmail.com>
2020-12-23 18:50:39 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Month from "./Components/Month";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+   state = {
+       months : [ { id:1, completed: 'false',
+           pe:'', dpe: '', dbte:'', vle:'',
+           we:'', wpe: '', wbte:'', wle:'',
+           se:'', spe: '', sbte:'', sle:''
+       }
+       ]
+   }
 
+
+  render() {
+    return (
+        <div className= "App">
+          <div className="container">
+            <h1>Social payments </h1>
+
+            <table>
+                <tr>
+                    <th rowSpan="2" colSpan='1'>monthes</th>
+              <th  colSpan="4">Electricity</th>
+              <th colSpan="4">Water</th>
+              <th colSpan="4">Head State</th>
+                </tr>
+                <tr>
+                    <th colSpan='1'>pay</th>
+                    <th colSpan='1'>debt</th>
+                    <th>date</th>
+                    <th>value</th>
+                    <th colSpan='1'>pay</th>
+                    <th colSpan='1'>debt</th>
+                    <th>date</th>
+                    <th>value</th>
+                    <th colSpan='1'>pay</th>
+                    <th colSpan='1'>debt</th>
+                    <th>date</th>
+                    <th>value</th>
+
+                </tr>
+
+                    {this.state.months.map(month => {
+                        return <Month month = {month }
+                        />
+                    })}
+
+
+
+
+            </table>
+
+
+
+          </div>
+
+
+
+        </div>
+
+
+
+
+
+
+    )
+  }
+}
 export default App;
---
+++ src/Components/Month.js
@@ -0,0 +1,60 @@
+import React  from 'react';
+
+
+
+
+function Month({month}) {
+
+return (
+    <tr>
+        <th className="monthMame">January</th>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+        <input type="text" value={month} />
+        </td>
+         <td>
+            <input type="text" value={month} />
+         </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+        <td>
+            <input type="text" value={month} />
+        </td>
+
+
+
+
+
+
+
+
+    </tr>
+  )
+}
+export default Month;
+
+
